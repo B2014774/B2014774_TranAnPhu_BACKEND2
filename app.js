fd800efd91a201b87ac28162be8a6f2774bfe5c3
Phu Tran <phub2014774@student.ctu.edu.vn>
2023-09-20 07:39:21 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
@@ -12,5 +13,21 @@ app.get ("/", (req,res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req,res,next) => {
+    //Code se chay khi khong co router duoc dinh nghia
+    return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handling middleware last, after other app.use() and routes calls
+app.use((err,req,res,next) => {
+    //Middleware xu ly loi tap trung
+    // Trong cac doan code xu ly cac route, goi next(error)
+    // Se chuyen ve middleware xu ly loi nay
+   return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+   });
+});
 
 module.exports = app;
